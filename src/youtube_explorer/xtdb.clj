04d3d967535ac0d6fb1d6aff847c2785cc8fd95e
Ben Follington <5009316+bfollington@users.noreply.github.com>
2022-08-02 16:57:38 +1000

    Add xtdb with postgres storage

---
+++ src/youtube_explorer/xtdb.clj
@@ -0,0 +1,53 @@
+(ns youtube-explorer.xtdb
+  (:require [clojure.java.io :as io]
+            [mount.core :refer [defstate]]
+            [xtdb.api :as xt]
+            [clojure.edn :as edn])
+  (:import (java.time Duration)))
+
+
+(defn load-edn
+  "Load edn from an io/reader source (filename or io/resource)."
+  [source]
+  (try
+    (with-open [r (io/reader source)]
+      (edn/read (java.io.PushbackReader. r)))
+
+    (catch java.io.IOException e
+      (printf "Couldn't open '%s': %s\n" source (.getMessage e)))
+    (catch RuntimeException e
+      (printf "Error parsing edn file '%s': %s\n" source (.getMessage e)))))
+
+(def db-spec
+  (load-edn "db.edn"))
+
+(defn start-xtdb! []
+  (xt/start-node
+   {:xtdb/tx-log {:xtdb/module 'xtdb.jdbc/->tx-log
+                  :connection-pool {:dialect {:xtdb/module 'xtdb.jdbc.psql/->dialect}
+                                    :pool-opts {}
+                                    :db-spec db-spec}
+                  :poll-sleep-duration (Duration/ofSeconds 1)}
+    :xtdb/document-store {:xtdb/module 'xtdb.jdbc/->document-store
+                          :connection-pool {:dialect {:xtdb/module 'xtdb.jdbc.psql/->dialect}
+                                            :pool-opts {}
+                                            :db-spec db-spec}}}))
+
+
+(defstate xtdb-node
+  :start (start-xtdb!)
+  :stop (.close xtdb-node))
+
+(comment
+  (xt/submit-tx xtdb-node [[::xt/put
+                            {:xt/id "hi2u"
+                             :user/name "zig"}]]))
+
+(comment
+  (xt/q (xt/db xtdb-node) '{:find [e]
+                            :where [[e :user/name "zig"]]}))
+
+(def tx-details
+  (xt/submit-tx xtdb-node [[::xt/put
+                            {:xt/id "hi2u"
+                             :user/name "zig"}]]))
