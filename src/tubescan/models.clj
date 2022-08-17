943b8c4aa172abee284d25d51edf64d611ab622d
Zaymon Foulds-Cook <12402727+ZaymonFC@users.noreply.github.com>
2022-08-17 17:00:25 +1000

    🔨 Model recipes more like queries

--- src/tubescan/models.clj
+++ src/tubescan/models.clj
@@ -1,20 +1,26 @@
 (ns tubescan.models)
 
-(def ListChannelStep
+(def ChannelStep
   [:map
    [:name string?]
    [:after-date {:optional true} string?]
    [:order {:optional true} string?]
    [:max-results {:optional true} int?]])
 
+(def ChannelsStep
+  [:sequential string?])
+
 (def Location
-  [:map [:lat double?] [:long double?] [:radius {:optional true} int?]])
+  [:tuple {:title "location"} :double :double :radius])
+
+(def CaptionOptions
+  [:enum [:closed-caption]])
 
 (def SearchStep
   [:map
    [:query string?]
    [:relevance-language {:optional true} string?]
-   [:captions {:optional true} keyword?]
+   [:captions {:optional true} CaptionOptions]
    [:location {:optional true} Location]
    [:published-after {:optional true} string?]
    [:order {:optional true} keyword?]
@@ -32,3 +38,6 @@
           :minimum-length
           :not-rated-by-me
           :excluding-channels]])
+
+(def FromOption
+  [:enum [:search :channel :channels :playlist :enhanced-playlist]])
